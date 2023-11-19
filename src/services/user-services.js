24631d9a6bc714338cd7a0cfed42161001f15101
timurturbil <timurturbill@gmail.com>
2023-11-19 18:11:34 +0300

    update user service endpoint

--- src/services/user-services.js
+++ src/services/user-services.js
@@ -1,9 +1,7 @@
 import { API_URL } from "../constants/api";
 import { get } from "../helpers/api-helper";
-import { getCookie } from "../helpers/cookie_helper";
 
 export const getCurrentUser = async () => {
-  const userId = getCookie("user_id");
-  const response = await get(API_URL + "/user/" + userId);
+  const response = await get(API_URL + "/users/me");
   return response;
 };
